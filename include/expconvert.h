80846c5a68f2209588eb003b1b5096516d8e3834
Cooder <cooder_z@outlook.com>
2022-08-27 18:39:02 +0800

    toPostFix已完成，待测试

--- include/expconvert.h
+++ include/expconvert.h
@@ -7,29 +7,43 @@
 #include<iostream>
 #include<stack>
 #include<new>
+#include<cstring>
+#include<string>
+#include<algorithm>
 
 class Expconvert
 {
 private:
-    char* infix;
+    std::string infix;
     int len;
 
+    std::string splitStr(char spilm);
+
+    /**
+     * @brief 返回操作符所在的优先级
+     * @param opt 操作符
+     * @return 等级标志，从0开始
+     * */
+    int optlevel(char opt);
+
+
+    std::string getstr(char splim);
 public:
-    Expconvert(): infix(nullptr), len(0) {}
-    Expconvert(char* _infix)
+    Expconvert(): infix(" "), len(0) {}
+    Expconvert(std::string _infix)
     {
         infix = _infix;
-        len = sizeof _infix;
+        len = _infix.length();
     }
 
     /**
      * @brief 将中缀表达式转换为后缀表达式
      * */
-    char* toPostfix();
+    std::string toPostfix();
 
     /**
      * @brief 将中缀表达式转换为前缀表达式
      * */
-    char* toPrefix();
+    std::string toPrefix();
 };
 #endif //INFIXTOPOSTFIXEXPRESSION_EXPCONVERT_H
--- src/expconvert.cpp
+++ src/expconvert.cpp
@@ -3,24 +3,147 @@
 //
 #include"expconvert.h"
 
-char* Expconvert::toPostfix()
-{
-    std::stack<char> opts;
-    char* postfix = new char[len];
-    char get;
-    for(int i = 0; i < len; i++)
-    {
-        get = infix[i];
-        if(get == '(')
-        {
-            opts.push(get);
-        }
-        if()
-    }
 
+int Expconvert::optlevel(char opt) {
+    char opts[] {'(', ')', '^', '*', '/', '+', '-'};
+    int optflag = -1;
+    char* optpos = std::find(opts, opts+6, opt);
+    if(*optpos == '-' && opt !='-')
+    {
+        return -1;
+    }
+    switch(*optpos)
+    {
+        case '(':
+        case ')':
+            optflag = 0;
+            break;
+        case '^':
+            optflag = 1;
+            break;
+        case '*':
+        case '/':
+            optflag = 2;
+            break;
+        case '+':
+        case '-':
+            optflag = 3;
+            break;
+    }
+    return optflag;
 }
 
-char *Expconvert::toPrefix()
+std::string Expconvert::getstr(char splim)
 {
+    static std::stack<int> pos;
+    std::string sample = infix;
+    std::string result;
+    if(pos.empty())
+    {
+        pos.push(0);
+    }
+    if(pos.top() == sample.length() - 1)
+    {
+        return "er";
+    }
+    int nowpos = pos.top();
+    while(sample[nowpos] != splim && nowpos <= sample.length() - 1)
+    {
+        nowpos++;
+    }
+    result = sample.substr(pos.top(), nowpos);
+    pos.pop();
+    pos.push(++nowpos);
+    return result;
+}
 
+std::string Expconvert::toPostfix()
+{
+    std::stack<char> opts;
+    std::string nowstr = getstr(' ');
+    std::string postfix;
+    do
+    {
+        // is operator
+        int optlv;
+        if( (optlv = optlevel(nowstr[0])) != -1)
+        {
+            switch(optlv)
+            {
+                case 0:
+                    if(nowstr == ")")
+                    {
+                        char nowopt;
+                        while((nowopt = opts.top()) != '(')
+                        {
+                            postfix += " ";
+                            postfix += nowopt;
+                            opts.pop();
+                        }
+                        //pop (
+                        opts.pop();
+                        break;
+                    }
+                    else if(nowstr == "(")
+                        opts.push(nowstr[0]);
+                    break;
+                case 1:
+                    opts.push(nowstr[0]);
+                    break;
+                case 2:
+                    if(optlevel(opts.top()) >= 2)
+                    {
+                        opts.push(nowstr[0]);
+                    }
+                    else if(optlevel(opts.top()) <= 1)
+                    {
+                        char pushopt = opts.top();
+                        while(pushopt != '(')
+                        {
+                            postfix += " ";
+                            postfix += pushopt;
+                            opts.pop();
+                            if(!opts.empty())
+                            {
+                                pushopt = opts.top();
+                            }
+                            else
+                                break;
+                        }
+                        opts.push(nowstr[0]);
+                    }
+                case 3:
+                    if(optlevel(opts.top()) == 3)
+                    {
+                        opts.push(nowstr[0]);
+                    }
+                    else
+                    {
+                        char pushopt = opts.top();
+                        while(pushopt != '(')
+                        {
+                            postfix += " ";
+                            postfix += pushopt;
+                            opts.pop();
+                            if(!opts.empty())
+                            {
+                                pushopt = opts.top();
+                            }
+                            else
+                                break;
+                        }
+                        opts.push(nowstr[0]);
+                    }
+            }
+        }
+        // is operant
+        else if(nowstr != " ")
+        {
+            postfix += " ";
+            postfix += nowstr;
+        }
+        nowstr = getstr(' ');
+    }while(nowstr != "er");
+
+    return postfix;
 }
